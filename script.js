9d4c3ea0a77886d567750602fa5951a8c3fbdf31
IBYIMANA <filletteibyimana@gmail.com>
2024-03-17 17:27:08 +0200

    4

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+module.exports = {
+    theme: {
+      borderRadius: {
+        'none': '0',
+        'sm': '0.125rem',
+        DEFAULT: '0.25rem',
+        'md': '0.375rem',
+        'lg': '0.5rem',
+        'full': '9999px',
+        'large': '12px',
+      }
+    }
+ }
